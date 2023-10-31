63027704a051df437130ee9300c2c6d12b0512e7
pcodesdev <njugunapm23@gmail.com>
2023-10-31 23:00:05 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Pizza from './pizza'
+
+function App () {
+  return (
+    <div>
+    <Pizza />
+    <Pizza />
+    <Pizza />
+    </div>
+  )
+}
+
+export default App
